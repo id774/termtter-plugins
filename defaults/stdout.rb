f3c1778d6a813d1500dc15897c1f05ce1fac318a
id774 <idnanashi@gmail.com>
2010-08-24 16:27:15 +0900

    #9 Maintain default sweets

--- defaults/stdout.rb
+++ defaults/stdout.rb
@@ -21,7 +21,7 @@ config.plugins.stdout.set_default(
     '</36>'
   ].join('')
 )
-config.plugins.stdout.set_default(:sweets, %w[Sally_0601 kkkn])
+config.plugins.stdout.set_default(:sweets, %w[Sally_0601])
 config.plugins.stdout.set_default(:sweet_color, 'cyan')
 config.plugins.stdout.set_default(:time_format_today, '%H:%M:%S')
 config.plugins.stdout.set_default(:time_format_not_today, '%y/%m/%d %H:%M')
