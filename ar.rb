02ac8c608924d6ba166ed6ca871b124a98b10009
id774 <idnanashi@gmail.com>
2011-02-14 03:38:58 +0900

    #16 Integrated status table

--- ar.rb
+++ ar.rb
@@ -9,15 +9,12 @@ ActiveRecord::Base.establish_connection(
 )
 
 class Status < ActiveRecord::Base
-  belongs_to :user
-end
-class User < ActiveRecord::Base
-  has_many :statuses
 end
 
 unless Status.table_exists?()
   ActiveRecord::Migration.create_table :statuses do |t|
-    t.column :user_id, :integer
+    t.column :uid, :integer
+    t.column :screen_name, :string
     t.column :text, :string
     t.column :created_at, :string
     t.column :in_reply_to_status_id, :integer
@@ -27,44 +24,25 @@ unless Status.table_exists?()
   end
 end
 
-unless User.table_exists?()
-  ActiveRecord::Migration.create_table :users do |t|
-    t.column :screen_name, :string
-    t.column :protected, :boolean
-    t.column :uid, :integer
-  end
-end
-
 module Termtter
   module Client
     register_hook(:collect_statuses_for_db, :point => :pre_filter) do |statuses, event|
       statuses.each do |s|
-        # Save users
-        unless User.exists?(s.user.id)
-          user = {}
-          User.columns.map{|x| x.name.intern }.each do |col|
-            user[col] =
-              if event.class == SearchEvent && col == :protected
-                false
-              elsif col == :uid
-                s.user.id
-              elsif col != :id
-                s.user[col]
-              end
-          end
-          User.create(user)
-        end
-
-        # Save statuses
         unless Status.exists?(s.id)
           status = {}
           Status.columns.map{|x| x.name.intern }.each do |col|
             status[col] =
-              col != :user_id ? (s[col] rescue nil) : nil
+              if col == :protected
+                s.protected
+              elsif col == :uid
+                s.user.id
+              elsif col == :screen_name
+                s.user[col]
+              else
+                s[col]
+              end
           end
-          u = User.find(:first,
-                        :conditions => ['uid = :i', {:i => s.user.id}])
-          u.statuses.create(status)
+          Status.create(status)
         end
       end
     end
@@ -81,7 +59,6 @@ module Termtter
 
     register_command(:db_clear) do |arg|
       if confirm('Are you sure?')
-        User.delete_all
         Status.delete_all
       end
     end
@@ -90,8 +67,7 @@ module Termtter
       user_name = normalize_as_user_name(arg)
       statuses = Status.find(
         :all,
-        :joins      => "LEFT OUTER JOIN users ON users.uid = statuses.user_id",
-        :conditions => ['users.screen_name = :u',{:u => user_name}],
+        :conditions => ['statuses.screen_name = :u',{:u => user_name}],
         :limit      => 20)
       output(statuses, :db_search)
     end
