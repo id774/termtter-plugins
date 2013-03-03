72d55947caa946a91a1662001a7f6c2cd1d05dda
id774 <idnanashi@gmail.com>
2013-03-03 17:35:38 +0900

    36 Append spec for ar plugin

--- spec/plugins/ar_spec.rb
+++ spec/plugins/ar_spec.rb
@@ -9,7 +9,7 @@ module Termtter
       Termtter::Client.plug 'ar'
     end
 
-    it 'self.insert should not return false' do
+    it 'self.save should not return false' do
       Termtter::Client.plug 'ar'
       @status = Status.new
       @status.screen_name = 'hoge'
@@ -22,5 +22,18 @@ module Termtter
       @status.source = 'Termtter tests'
       @status.save.should_not be_false
     end
+
+    it 'saved record should be equal the origin record' do
+      status_find = Status.find(:first)
+      status_find.screen_name.should == 'hoge'
+      status_find.id_str.should == '55555'
+      status_find.text.should == 'ほげ'
+      status_find.protected.should be_true
+      status_find.statuses_count.should == 500
+      status_find.friends_count.should == 1000
+      status_find.followers_count.should == 1500
+      status_find.source.should == 'Termtter tests'
+    end
+
   end
 end
