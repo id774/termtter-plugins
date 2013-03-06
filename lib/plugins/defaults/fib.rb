1eceb0349d16de5e1570cdba73ee18521d5c2721
id774 <idnanashi@gmail.com>
2013-03-06 16:20:17 +0900

    41 Remove plugins using obsolete api

--- lib/plugins/defaults/fib.rb
+++
@@ -1,8 +0,0 @@
-require 'set'
-
-module Termtter::Client
-  help = ['favorites,favlist,f USERNAME', 'show user favorites']
-  Termtter::Client.register_command(:favorites, :alias => :f, :help => help) do |arg|
-    output(Termtter::API.twitter.favorites(arg), Termtter::Event.new(:user_timeline, :type => :favorite))
-  end
-end
--- spec/plugins/default_replace_spec.rb
+++ spec/plugins/default_replace_spec.rb
@@ -4,7 +4,7 @@ require File.expand_path(File.dirname(__FILE__)) + '/../spec_helper'
 
 describe Termtter do
   it 'plugin default_replace' do
-    Termtter::Client.should_receive(:register_command).at_least(1).times
+    Termtter::Client.should_receive(:register_command).exactly(0).times
     Termtter::Client.plug 'default_replace'
   end
 end
